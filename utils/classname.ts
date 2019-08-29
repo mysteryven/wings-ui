ac12048e01dd9916fb9c9ad54b8b88e09b8bf66a
mysteryven <mysteryven@gmail.com>
2019-08-29 13:56:21 +0800

    refactor:Move utils to src

--- utils/classname.ts
+++
@@ -1,5 +0,0 @@
-const classname = function(...classes: (string | undefined)[]) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export default classname
--- utils/testUtils.ts
+++
@@ -1,4 +0,0 @@
-import * as Enzyme from 'enzyme';
-export const findByTestAttr = (wrapper:Enzyme.ShallowWrapper, val:string) => {
-  return wrapper.find(`.${val}`);
-};
