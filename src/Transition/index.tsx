7811a73d01b3e7b11c4afae8b3f8c935c30c8b76
mysteryven <mysteryven@gmail.com>
2019-09-02 18:21:51 +0800

    feat:transition components

--- src/Transition/index.tsx
+++ src/Transition/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import {CSSProperties, FunctionComponent} from "react";
+import { useState, useEffect, useRef } from 'react'
+import { CSSProperties, FunctionComponent } from "react";
+import './index.scss';
 
 interface TransitionProps {
   before?: CSSProperties;
@@ -7,10 +9,29 @@ interface TransitionProps {
 }
 
 const Transition: FunctionComponent<TransitionProps> = (props) => {
+  const divEl = useRef<null | HTMLDivElement>(null);
+  const [isDisplay, setDisplay] = useState<'none' | 'block'>('none');
+  useEffect(() => {
+    setDisplay('block');
+    if (divEl) {
+      divEl.current.getBoundingClientRect()
+    }
+    const child = React.Children.only(props.children);
+    const style = [child.props.style, { opacity: 0 }];
+    React.cloneElement(
+      props.children, {
+
+      }
+    )
+  })
 
   return (
-    <div className={"w-transition"}>
-      hi
+    <div
+      className={"w-transition"}
+      style={{ display: isDisplay }}
+      ref={divEl}
+    >
+      {props.children}
     </div>
   )
 };
