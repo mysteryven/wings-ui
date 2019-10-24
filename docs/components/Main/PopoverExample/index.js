082811ce68ef1e3af3b38da5f0bed0c2f4f695e7
wenzhe <mysteryven@gmail.com>
2019-10-24 15:47:01 +0800

    feat:move content to body, add bottom position

--- docs/components/Main/PopoverExample/index.js
+++ docs/components/Main/PopoverExample/index.js
@@ -5,14 +5,22 @@ import Button from '../../../../src/Button';
 const PopoverExample = () => {
   return (
     <>
-    <div style={{ marginTop: 100, marginLeft: 100 }}>
-      <Popover content="我问一下，你今天快乐吗">
-        <Button>xxxxxxxx</Button>
+    <div style={{ marginTop: 60, marginLeft: 100 }}>
+      <h5>点击触发</h5> 
+      <Popover content="我问一下，你今天快乐吗，我感觉你非常的快乐？">
+        <Button>Top</Button>
+      </Popover>
+      <Popover position="bottom" content="我问一下，你今天快乐吗，我感觉你非常的快乐？">
+        <Button>Bottom</Button>
       </Popover>
     </div>
-    <div style={{ marginTop: 100, marginLeft: 100 }}>
+    <div style={{ marginTop: 60, marginLeft: 100 }}>
+      <h5>长按触发</h5> 
       <Popover type="longPress" content="我问一下，你今天快乐吗">
-        <Button>xxxxxxxx</Button>
+        <Button>Top</Button>
+      </Popover>
+      <Popover type="longPress" position="bottom" content="我问一下，你今天快乐吗">
+        <Button>Bottom</Button>
       </Popover>
     </div>
     </>
--- src/Popover/index.tsx
+++ src/Popover/index.tsx
@@ -1,10 +1,27 @@
 import React, { useRef, useState, useEffect, RefObject } from 'react';
+import sc from '../utils/classname'
 import './index.scss';
+import { CSSProperties } from 'react';
+import * as ReactDOM from 'react-dom';
 
 interface PopoverProps {
   content: React.ReactNode | string;
   type?: 'shortPress' | 'longPress';
   interval?: number;
+  position?: 'top' | 'bottom';
+}
+
+type PositionItem = string | number;
+
+interface Position {
+  'top': {
+    left: PositionItem,
+    bottom: PositionItem
+  },
+  'bottom': {
+    left: PositionItem,
+    top: PositionItem
+  }
 }
 
 let timer: any = null;
@@ -12,10 +29,11 @@ let timer: any = null;
 const Popover: React.FC<PopoverProps> = (props) => {
   const triggerEl = useRef<null | HTMLDivElement>(null);
   const contentEl = useRef<null | HTMLDivElement>(null);
-  const [offsetX, setOffsetX] = useState<number>(0);
+  const [offset, setOffset] = useState<CSSProperties>({});
   const [hasSetOffset, setOffsetStatus] = useState<boolean>(false);
   const [isVisible, setVisible] = useState<boolean>(false);
   const [shouldRender, setShouldRender] = useState<boolean>(false);
+  const position = props.position || 'top';
 
   useEffect(() => {
     if (isVisible) {
@@ -29,7 +47,21 @@ const Popover: React.FC<PopoverProps> = (props) => {
 
   useEffect(() => {
     if (triggerEl && triggerEl.current && contentEl && contentEl.current) {
-      setOffsetX(( getWidth(triggerEl) - getWidth(contentEl)) / 2)
+      let { left, top, width, height, bottom } = triggerEl.current.getBoundingClientRect();
+      let { width: contentWidth } = contentEl.current.getBoundingClientRect();
+
+      let positions: Position = {
+        top: {
+          left: left + window.scrollX - (contentWidth - width) / 2,
+          bottom: document.documentElement.scrollHeight - top + 8
+        },
+        bottom: {
+          left: left + window.scrollX - (contentWidth - width) / 2,
+          top: bottom + window.scrollY 
+        } 
+      }
+
+      setOffset(positions[position]);
       setOffsetStatus(true);
       document.addEventListener('click', onDocumentClick)
     }
@@ -73,24 +105,18 @@ const Popover: React.FC<PopoverProps> = (props) => {
     }
   }
 
-  function getWidth(el: RefObject<HTMLDivElement>) {
-    if (el && el.current) {
-      return Number.parseFloat(window.getComputedStyle(el.current).width as string);
-    }
-    return 0;
-  }
-
   return (
     <div className="w-popover">
       {
-        shouldRender && (
-          <div className="w-popover-content" ref={contentEl} style={{
-            left: offsetX,
+        shouldRender && ReactDOM.createPortal(
+          <div 
+            className={sc("w-popover-content", `w-popover-content-${position}`)} ref={contentEl} style={{
+            ...offset,
             visibility: hasSetOffset ? 'visible' : 'hidden'
           }}>
             {props.content}
           </div>
-        )
+          , document.body)
       }
       <div
         className="w-popover-trigger"
@@ -110,6 +136,7 @@ const Popover: React.FC<PopoverProps> = (props) => {
 Popover.defaultProps = {
   interval: 300,
   type: 'shortPress',
+  position: 'top',
 }
 
 export default Popover;
