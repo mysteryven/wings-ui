b9a28254fb2da280ca0b2b0d431f54b1967e3dbb
wenzhe <mysteryven@gmail.com>
2019-10-24 11:01:06 +0800

    feat:popover basics

--- docs/components/Main/PopoverExample/index.js
+++ docs/components/Main/PopoverExample/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Popover from '../../../../src/Popover'
+import Popover from '../../../../src/Popover';
+import Button from '../../../../src/Button';
 
 const PopoverExample = () => {
   return ( 
-    <div>
-      <Popover />
+    <div style={{ marginTop: 100, marginLeft: 100 }}>
+      <Popover content="我问一下，你今天快乐吗">
+        <Button>xxxxxxxx</Button>
+      </Popover>
     </div>
   ) 
 }
--- src/Popover/index.tsx
+++ src/Popover/index.tsx
@@ -1,14 +1,68 @@
-import * as React from 'react';
+import React, { useRef, useState, useEffect, RefObject } from 'react';
+import './index.scss';
+import { EventType } from '@testing-library/dom';
 
 interface PopoverProps {
   content: React.ReactNode | string;
 }
 
 const Popover: React.FC<PopoverProps> = (props) => {
+  const triggerEl = useRef<null | HTMLDivElement>(null);
+  const contentEl = useRef<null | HTMLDivElement>(null);
+  const [offsetX, setOffsetX] = useState<number>(0);
+  const [isVisible, setVisible] = useState<boolean>(false);
+  const [shouldRender, setShouldRender] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (triggerEl && triggerEl.current && contentEl && contentEl.current) {
+      const triggerWidth = getWidth(triggerEl);
+      const contentWidth = getWidth(contentEl);
+
+      setOffsetX((triggerWidth - contentWidth) / 2)
+      document.addEventListener('click', onDocumentClick)
+    }
+
+    return () => {
+      document.removeEventListener('click', onDocumentClick);
+    }
+  })
+
+  useEffect(() => {
+    setShouldRender(isVisible);
+    !isVisible && document.removeEventListener('click', onDocumentClick);
+  }, [isVisible])
+
+  function onDocumentClick(e: any) {
+    if (isInner(contentEl)) {
+      return;
+    }
+    
+    setVisible(false);
+
+    function isInner(el: RefObject<HTMLDivElement>) {
+      return el && el.current && el.current.contains(e.target)
+    }
+  }
+
+  function getWidth(el: RefObject<HTMLDivElement>) {
+    if (el && el.current) {
+      return Number.parseFloat(window.getComputedStyle(el.current).width as string);
+    }
+    return 0;
+  }
+
   return (
     <div className="w-popover">
-      <div className="w-popover-content">{props.content}</div>
-      {props.children}
+      {
+        shouldRender && (
+          <div className="w-popover-content" ref={contentEl} style={{ left: offsetX }}>
+            {props.content}
+          </div>
+        )
+      }
+      <div className="w-popover-trigger" ref={triggerEl} onClick={() => setVisible(!isVisible)}>
+        {props.children}
+      </div>
     </div>
   )
 }
